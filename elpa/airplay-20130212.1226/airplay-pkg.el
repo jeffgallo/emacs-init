0ae735adb6f50316161f1a2ab17f64ae71eedda6
Jeff Gallo <jgallo@strmln.work>
2019-03-07 08:57:28 -0500

    initial commit

---
+++ elpa/airplay-20130212.1226/airplay-pkg.el
@@ -0,0 +1,14 @@
+(define-package "airplay" "20130212.1226" "Airplay bindings to Emacs"
+  '((request "20130110.2144")
+    (simple-httpd "1.4.1")
+    (deferred "0.3.1"))
+  :keywords
+  '("appletv" "airplay")
+  :authors
+  '(("Wataru MIYAGUNI" . "gonngo@gmail.com"))
+  :maintainer
+  '("Wataru MIYAGUNI" . "gonngo@gmail.com")
+  :url "https://github.com/gongo/airplay-el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
